886c0a1cdab3890fb88140538937c40b7a51863d
YerimYerim <vigvang_bip@naver.com>
2024-03-11 00:59:48 +0900

    [yerim] tooltip 위치 커밋

--- ProjectW/Assets/Script/UI/UITooltip.cs
+++ ProjectW/Assets/Script/UI/UITooltip.cs
@@ -30,33 +30,28 @@ public class UITooltip : UIBase
         txtDesc.text = GameUtil.GetString(descStringKey);
 
         //var tooltipPosition = toolTipRectTransform.anchoredPosition3D;
-        var parentsRectPosition = parentsRect.anchoredPosition3D;
+        var parentsRectPosition = parentsRect.position;
 
         float x = 0f;
         float y = 0f;
         
         
         // 해당 아이템이 화면의 왼쪽에 있을경우 오른쪽에
-        if (parentsRectPosition.x < 0)
-        {
-            x = parentsRectPosition.x - Screen.width * 0.5f - parentsRect.sizeDelta.x * 0.5f - toolTipRectTransform.sizeDelta.x * 0.5f;
-        }
-        else
-        {
-            x = parentsRectPosition.x - Screen.width * 0.5f - parentsRect.sizeDelta.x * 0.5f + toolTipRectTransform.sizeDelta.x * 0.5f;
-        }
+
+        x = parentsRectPosition.x + Math.Abs(parentsRect.rect.x * 0.5f) + Math.Abs(toolTipRectTransform.rect.x * 0.5f);
+        
 
         // 해당 아이템이 화면하단에 있을경우 위쪽에 
         if (parentsRect.position.y < 0)
         {
-            y = parentsRectPosition.y - Screen.height * 0.5f;
+            y = parentsRectPosition.y;// + Math.Abs(parentsRect.rect.y * 0.5f) + Math.Abs(toolTipRectTransform.rect.y * 0.5f);
         }
         else
         {
-            y = parentsRectPosition.y - Screen.height * 0.5f;
+            y = parentsRectPosition.y; //  - Screen.height * 0.5f;
         }
 
-        toolTipRectTransform.anchoredPosition = new Vector3(x, y);
+        toolTipRectTransform.position = new Vector3(parentsRectPosition.x +  Math.Abs(toolTipRectTransform.rect.x * 0.5f), y - Math.Abs(toolTipRectTransform.rect.y * 0.5f) +  Math.Abs(parentsRect.rect.y * 0.5f) + 10 );
     }
     
     public override void Show()

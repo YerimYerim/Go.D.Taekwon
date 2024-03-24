c4e960c9473464d20a0003a7258f997912c142e8
liloxx <lilox@naver.com>
2024-03-24 19:05:51 +0900

    [woo] 전투 기본 테이블 - actor, spell_effect 테이블 추가 및 가데이터 구성 / 몬스터 체력 바 프리팹 추가

--- ProjectW/Assets/Script/Enum.cs
+++ ProjectW/Assets/Script/Enum.cs
@@ -2,3 +2,12 @@ public enum LANGUAGE_TYPE
 {
      LANGUAGE_TYPE_KOR = 0,
 }
+public enum ACTOR_TYPE
+{
+     ACTOR_TYPE_PLAYER = 0,
+     ACTOR_TYPE_MONSTER = 1,
+}
+public enum EFFECT_TYPE
+{
+     EFFECT_TYPE_DAMAGE = 0,
+}

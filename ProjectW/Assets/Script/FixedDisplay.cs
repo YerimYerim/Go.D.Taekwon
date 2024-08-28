9aaa59277314083c5a50da7dfdf1581a4c3859cd
YerimYerim <vigvang_bip@naver.com>
2024-08-29 01:50:11 +0900

    [yerim] 화면 fixed display 제거
    - 어차피 고정비 pc 기반 게임이라 필요없어서 삭제

--- ProjectW/Assets/Script/FixedDisplay.cs
+++ ProjectW/Assets/Script/FixedDisplay.cs
@@ -6,53 +6,53 @@ namespace Script
 {
     public class FixedDisplay : MonoBehaviour
     {
-        private int deviceWidth;
-        private int deviceHeight;
-        public void Awake()
-        {
-            SetResolution(); // 초기에 게임 해상도 고정
-        }
-
-        private void SetUpCanvasScaler(int setWidth, int setHeight)
-        {
-            CanvasScaler canvasScaler = FindObjectOfType<CanvasScaler>();
-            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-            canvasScaler.referenceResolution = new Vector2(setWidth, setHeight);
-            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
-        }
-
-        public void SetResolution(int setWidth = 1920, int setHeight = 1080)
-        {
-            SetUpCanvasScaler(setWidth, setHeight);
-
-            if(deviceWidth.Equals(width) == false)
-            {
-                deviceWidth = width; // 기기 너비 저장
-            }
-
-            if (deviceHeight.Equals(height) == false)
-            {
-                deviceHeight = height; // 기기 높이 저장
-            }
-
-            //Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
-
-            if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight)
-            {
-                float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight);
-                if (Camera.main != null)
-                {
-                    Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
-                }
-            }
-            else // 게임의 해상도 비가 더 큰 경우
-            {
-                float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight);
-                if (Camera.main != null)
-                {
-                    Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
-                }
-            }
-        }
+        // private int deviceWidth;
+        // private int deviceHeight;
+        // public void Awake()
+        // {
+        //     SetResolution(); // 초기에 게임 해상도 고정
+        // }
+        //
+        // private void SetUpCanvasScaler(int setWidth, int setHeight)
+        // {
+        //     CanvasScaler canvasScaler = FindObjectOfType<CanvasScaler>();
+        //     canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        //     canvasScaler.referenceResolution = new Vector2(setWidth, setHeight);
+        //     canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+        // }
+        //
+        // public void SetResolution(int setWidth = 1920, int setHeight = 1080)
+        // {
+        //     SetUpCanvasScaler(setWidth, setHeight);
+        //
+        //     if(deviceWidth.Equals(width) == false)
+        //     {
+        //         deviceWidth = width; // 기기 너비 저장
+        //     }
+        //
+        //     if (deviceHeight.Equals(height) == false)
+        //     {
+        //         deviceHeight = height; // 기기 높이 저장
+        //     }
+        //
+        //     //Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
+        //
+        //     if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight)
+        //     {
+        //         float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight);
+        //         if (Camera.main != null)
+        //         {
+        //             Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
+        //         }
+        //     }
+        //     else // 게임의 해상도 비가 더 큰 경우
+        //     {
+        //         float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight);
+        //         if (Camera.main != null)
+        //         {
+        //             Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
+        //         }
+        //     }
+        // }
     }
 }
